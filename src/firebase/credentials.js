8b12ab1857949f6e2f19b2c378920baaaa321719
AlejandroJesusSerrano <alejandrojserrano@hotmail.com>
2022-08-24 13:13:13 -0300

    se quita archivo .env para presentacion de trabnajo final

--- src/firebase/credentials.js
+++ src/firebase/credentials.js
@@ -1,15 +1,20 @@
+// Import the functions you need from the SDKs you need
 import { initializeApp } from "firebase/app";
 
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
 const firebaseConfig = {
-  apiKey: process.env.REACT_APP_FIREBASE_APIKEY,
-  authDomain: process.env.REACT_APP_FIREBASE_AUTHDOMAIN,
-  projectId: process.env.REACT_APP_FIREBASE_PROJECTID,
-  storageBucket: process.env.REACT_APP_FIREBASE_STORAGEBUCKET,
-  messagingSenderId: process.env.REACT_APP_FIREBASE_MESSAGINGSENDERID,
-  appId: process.env.REACT_APP_FIREBASE_APPID
+  apiKey: "AIzaSyB0q34QHq4ua81P1iraSca638PmSHUrIEQ",
+  authDomain: "coderhouse-ecommerce-95706.firebaseapp.com",
+  projectId: "coderhouse-ecommerce-95706",
+  storageBucket: "coderhouse-ecommerce-95706.appspot.com",
+  messagingSenderId: "228893995046",
+  appId: "1:228893995046:web:de6dfda038f2301ba036e7"
 };
 
-
+// Initialize Firebase
 const firebaseApp = initializeApp(firebaseConfig);
 
-export default firebaseApp
+export default firebaseApp;
